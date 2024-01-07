3edebd72ca058c36d17f2f77a932965e9ebb0beb
Milix-M <tujikou12dh@gmail.com>
2024-01-08 01:51:00 +0900

    add user model

---
+++ api/db/user_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from api.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    userid = Column(String)
+    username = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
