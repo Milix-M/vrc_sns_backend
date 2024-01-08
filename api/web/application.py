3f849a441584d0835aa596905bc3847d4b819581
Milix-M <tujikou12dh@gmail.com>
2024-01-08 23:38:26 +0900

    import startup_event & shutdown_event

--- api/web/application.py
+++ api/web/application.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from api.web.api.router import api_router
+from api.web.lifetime import register_startup_event, register_shutdown_event
 
 def get_app() -> FastAPI:
     """
