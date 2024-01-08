298896efcf7a1e4fb185d6a7f7346a335d26dc3a
Milix-M <tujikou12dh@gmail.com>
2024-01-09 04:18:58 +0900

    update user_dao!!

--- api/db/dao/user_dao.py
+++ api/db/dao/user_dao.py
@@ -1,14 +1,25 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from fastapi import Depends
+
+from api.db.dependencies import get_db_session
 from api.db.models.user_model import User
-from api.web.api.users import schema
-
 from api.libs.security import get_password_hash
 
+class UserDAO:
+    def __init__(self, session: AsyncSession = Depends(get_db_session)):
+        self.session = session
 
-def create_user(db: Session, user: schema.UserCreate):
-    db_user = User(email=user.email, username=user.username, userid=user.userid, hashed_password=get_password_hash(user.password))
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+    async def create_user(
+            self,
+            userid: str,
+            username: str,
+            email: str,
+            password: str
+        ) -> User:
+        user = User(userid=userid, username=username, email=email, hashed_password=get_password_hash(password))
+        self.session.add(user)
+        await self.session.commit()
+
+        return user
+        # todo 続きを書く
