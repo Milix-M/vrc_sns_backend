07d77ef1dc6ad3290b1792794a56ff2302cd5ae1
Milix-M <tujikou12dh@gmail.com>
2024-01-08 23:56:53 +0900

    change db settings

--- api/settings.py
+++ api/settings.py
@@ -1,3 +1,5 @@
+from yarl import URL
+
 class Settings():
     """
     Application Settings.
@@ -6,8 +8,27 @@ class Settings():
     port : int = 8000
     reload: bool = True
 
-    db_url: str = "sqlite:///sns_app.db"
+    db_host = ""
+    db_port = 0
+    db_user = ""
+    db_pass = ""
+    db_base = "//sns_app.db"
 
+    @property
+    def db_url(self) -> URL:
+        """
+        Assemble database URL from settings.
+
+        :return: database URL.
+        """
+        return URL.build(
+            scheme="sqlite",
+            host=self.db_host,
+            port=self.db_port,
+            user=self.db_user,
+            password=self.db_pass,
+            path=f"/{self.db_base}",
+        )
 
 
 settings = Settings()
