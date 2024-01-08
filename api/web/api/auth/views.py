a8d6948afe59d43fd630abfe680945bb11ed1f6a
Milix-M <tujikou12dh@gmail.com>
2024-01-09 04:19:27 +0900

    update views

--- api/web/api/auth/views.py
+++ api/web/api/auth/views.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Response
+
+from api.web.api.users.schema import User, UserCreate
+from api.db.dao.user_dao import UserDAO
 
 router = APIRouter()
 
 @router.post("/signin")
-async def create_user():
+async def create_user(
+    user: UserCreate,
+    user_dao: UserDAO = Depends(),
+    ) -> Response:
+    """
+    Create User endpoint.
+    """
+    await user_dao.create_user(
+        userid=user.userid,
+        username=user.username,
+        email=user.email,
+        password=user.password
+        )
     return
-
+    #todo 続きを書く
