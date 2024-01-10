b24316dc59ef6c5586e964b6a04f7d4ccba116ec
Milix-M <tujikou12dh@gmail.com>
2024-01-10 22:01:20 +0900

    update env.py

--- api/db/migrations/env.py
+++ api/db/migrations/env.py
@@ -9,11 +9,12 @@ from api.db.meta import meta
 from api.db.models import load_all_models
 from api.settings import settings
 
-load_all_models()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+
+load_all_models()
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -67,11 +68,11 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """
+    Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
     connectable = create_async_engine(str(settings.db_url))
 
