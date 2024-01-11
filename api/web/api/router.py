12205d30e1321b004a8d0a5bc4b886bde178a759
Milix-M <tujikou12dh@gmail.com>
2024-01-12 03:33:48 +0900

    include users router

--- api/web/api/router.py
+++ api/web/api/router.py
@@ -1,6 +1,7 @@
 from fastapi.routing import APIRouter
 
-from api.web.api import auth
+from api.web.api import auth, users
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
+api_router.include_router(users.router)
