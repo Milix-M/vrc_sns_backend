f8c02801de3e4796ffc2c2a607507fc4d33e2eef
Milix-M <tujikou12dh@gmail.com>
2024-01-14 21:55:32 +0900

    rename primary key

--- api/db/models/post_model.py
+++ api/db/models/post_model.py
@@ -5,7 +5,7 @@ from api.db.base import Base
 class Post(Base):
     __tablename__ = "posts"
 
-    id = Column(Integer, primary_key=True)
+    postid = Column(Integer, primary_key=True)
     userid = Column(Integer, ForeignKey("users.id", onupdate="CASCADE", ondelete="CASCADE"))
     content = Column(String(500))
     like = Column(Integer, default=0)
