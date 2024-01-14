e556d236c12e5c68195c845daa7918f6133642df
Milix-M <tujikou12dh@gmail.com>
2024-01-14 23:00:04 +0900

    add tags

--- api/web/api/router.py
+++ api/web/api/router.py
@@ -3,5 +3,5 @@ from fastapi.routing import APIRouter
 from api.web.api import auth, post
 
 api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth")
+api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 api_router.include_router(post.router, prefix="/post", tags=["Post"])
