3d69bebeb1f3fbdc6b3d0e4c3a4e66250004d356
Milix-M <tujikou12dh@gmail.com>
2024-01-14 22:52:03 +0900

    add tags

--- api/web/api/router.py
+++ api/web/api/router.py
@@ -4,4 +4,4 @@ from api.web.api import auth, post
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth")
-api_router.include_router(post.router, prefix="/post")
+api_router.include_router(post.router, prefix="/post", tags=["Post"])
