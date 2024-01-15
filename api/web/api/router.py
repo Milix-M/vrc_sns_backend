e1c90091c15f50afebd1e2cbe967b8d586a9a11f
Milix-M <tujikou12dh@gmail.com>
2024-01-16 02:47:40 +0900

    #11 include token.router & google.router

--- api/web/api/router.py
+++ api/web/api/router.py
@@ -1,7 +1,10 @@
 from fastapi.routing import APIRouter
 
-from api.web.api import auth, users
+from api.web.api import auth, users, token
+from api.web.api.auth import google
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
+api_router.include_router(token.router)
+api_router.include_router(google.router, prefix="/google", tags=["GoogleAuth"])
