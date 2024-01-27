58e915281f829e24e30d66da34e18fdee5bf66b1
Milix-M <tujikou12dh@gmail.com>
2024-01-27 23:58:11 +0900

    add users prefix

--- api/web/api/router.py
+++ api/web/api/router.py
@@ -6,6 +6,6 @@ api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 api_router.include_router(google.router, prefix="/google", tags=["GoogleAuth"])
-api_router.include_router(users.router)
+api_router.include_router(users.router, prefix="/users", tags=["User"])
 api_router.include_router(token.router)
 api_router.include_router(post.router, prefix="/post", tags=["Post"])
