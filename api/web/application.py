d1ad356986674a273aa591daabf3047fb5665498
Milix-M <tujikou12dh@gmail.com>
2024-01-27 23:55:53 +0900

    CORS対応した

--- api/web/application.py
+++ api/web/application.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from api.web.api.router import api_router
 from api.web.lifetime import register_startup_event, register_shutdown_event
+from api.settings import settings
 
 def get_app() -> FastAPI:
     """
@@ -15,5 +17,15 @@ def get_app() -> FastAPI:
     register_shutdown_event(app)
 
     app.include_router(router=api_router, prefix="/api")
+    app.add_middleware(
+        CORSMiddleware,
+        #許可するオリジンを指定
+        allow_origins=settings.origins,
+        #認証情報のアクセスを許可
+        allow_credentials=True,
+        # 全てのリクエストメソッドを許可
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
