60236c861be770826ab1a0746ae6b9940461a337
Milix-M <tujikou12dh@gmail.com>
2024-01-28 23:46:05 +0900

    ユーザーが初期登録されているか確認する関数を実装

--- api/web/api/users/views.py
+++ api/web/api/users/views.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 
+from api.db.dao.user_dao import UserDAO
 from api.web.api.users.schema import AuthenticatedUser
 from api.libs.middleware.auth import is_authenticated
 
@@ -10,3 +11,14 @@ async def user_me(
     user_info: AuthenticatedUser = Depends(is_authenticated),
 ) -> AuthenticatedUser:
     return user_info
+
+@router.get("/{id}/initialized", response_model=bool)
+async def user_initialized(
+    user_info: AuthenticatedUser = Depends(is_authenticated),
+    user_dao: UserDAO = Depends(),
+) -> bool:
+    """check user already initilized"""
+
+    userdata = await user_dao.get_user_by_id(id=user_info.id)
+
+    return userdata.is_initialized
