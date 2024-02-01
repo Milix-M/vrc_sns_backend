959b990fd215d9362ceec3814e85cf0078d31d66
Milix-M <tujikou12dh@gmail.com>
2024-02-02 03:16:38 +0900

    ルーティングに/timelineを追加

--- api/web/api/router.py
+++ api/web/api/router.py
@@ -1,5 +1,5 @@
 from fastapi.routing import APIRouter
-from api.web.api import auth, users, token, post
+from api.web.api import auth, users, token, post, timeline
 from api.web.api.auth import google
 
 api_router = APIRouter()
@@ -9,3 +9,4 @@ api_router.include_router(google.router, prefix="/google", tags=["GoogleAuth"])
 api_router.include_router(users.router, prefix="/users", tags=["User"])
 api_router.include_router(token.router)
 api_router.include_router(post.router, prefix="/post", tags=["Post"])
+api_router.include_router(timeline.router, prefix="/timeline", tags=["timeline"])
