d4b74eaa538ce27897ca4d051656a8d10f44d848
Milix-M <tujikou12dh@gmail.com>
2024-02-04 23:23:37 +0900

    post schemaの構造を大幅変更

--- api/web/api/post/schema.py
+++ api/web/api/post/schema.py
@@ -1,13 +1,19 @@
+import datetime
 from pydantic import BaseModel
 
-class PostBase(BaseModel):
+from api.web.api.users.schema import UserBase
+
+class PostCreate(BaseModel):
     content: str
 
-class Post(PostBase):
-    userid: int
+class Post(BaseModel):
     postid: int
-    favorite: int
-    repost: int
+    content: str
+    created_at: datetime.datetime
+    user: UserBase
+    userid: int
+    favorite_count: int
+    repost_count: int
 
 class PostID(BaseModel):
     postid: int
