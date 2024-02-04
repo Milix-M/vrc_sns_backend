257bafa93b5ffa03720590216a541e95bf40cef9
Milix-M <tujikou12dh@gmail.com>
2024-02-05 00:12:58 +0900

    update postのviewのget_post function

--- api/web/api/post/views.py
+++ api/web/api/post/views.py
@@ -51,19 +51,51 @@ async def add_post(
 
 @router.post("/show", response_model=Post)
 async def get_post(
-    post: PostID,
+    post_id: PostID,
     post_dao: PostDAO = Depends(),
-):
-    post_data = await post_dao.get_post_by_id(post)
+    user_dao: UserDAO = Depends(),
+) -> Post:
+    """
+    Retrieves a post by its ID and includes user information associated with the post.
+
+    Args:
+        post_id (PostID): The ID of the post to retrieve.
+        post_dao (PostDAO): The Data Access Object for posts, obtained through dependency injection.
+        user_dao (UserDAO): The Data Access Object for users, obtained through dependency injection.
+
+    Returns:
+        Post: The post with the specified ID, including content, creation date, associated user, and statistics like favorite and repost counts.
+
+    Raises:
+        HTTPException: If the post with the specified ID is not found.
+    """
+    post_data = await post_dao.get_post_by_id(post_id.postid)
 
     if post_data is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Post not found. Please check the post ID and try again."
         )
+    
+    # 返却するPostモデルにUserのデータが必要な為取ってくる
+    user_info = await user_dao.get_user_by_id(post_data.userid)
 
-    return await post_dao.get_post_by_id(
-        postid=post.postid
+    return Post(
+        postid=post_data.postid,
+        content=post_data.content,
+        created_at=post_data.created_at,
+        # Store user data within the Post data as well
+        user=UserBase(
+            id=user_info.id,
+            username=user_info.username,
+            userid=user_info.userid,
+            icon=user_info.icon,
+            headder=user_info.headder,
+            profile=user_info.profile
+        ),
+        userid=post_data.userid,
+        favorite_count=post_data.favorite_count,
+        repost_count=post_data.repost_count
     )
 
 
