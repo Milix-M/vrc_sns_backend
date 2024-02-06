afafe0725c5c66eca23dac761cfd241bedfde8f8
Milix-M <tujikou12dh@gmail.com>
2024-02-07 04:12:23 +0900

    add PostWOUser & UserPostsGet schema

--- api/web/api/post/schema.py
+++ api/web/api/post/schema.py
@@ -1,5 +1,5 @@
 import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from api.web.api.users.schema import UserBase
 
@@ -14,6 +14,23 @@ class Post(BaseModel):
     userid: int
     favorite_count: int
     repost_count: int
+
+class PostWOUser(BaseModel):
+    """
+    userのpost取得など,userの情報がすべて全く同じ時の為のschema
+    """
+    postid: int
+    content: str
+    created_at: datetime.datetime
+    favorite_count: int
+    repost_count: int
+
+class UserPostsGet(BaseModel):
+    userid: int
+    includeReplies: bool | None = Field(False, description="Whether to include replies or not")
+    limit: int | None = Field(15, description="Limit for fetching posts")
+    sinceid: int | None = Field(None)
+    untilid: int | None = Field(None)
 
 class PostID(BaseModel):
     postid: int
