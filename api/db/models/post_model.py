3c3b328574f04fb524b421b792d3d063a065cb4e
Milix-M <tujikou12dh@gmail.com>
2024-02-07 23:20:04 +0900

    update Post Model

--- api/db/models/post_model.py
+++ api/db/models/post_model.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from api.db.base import Base
+
+
+if TYPE_CHECKING:
+    from api.db.models.user_model import User
 
 
 class Post(Base):
@@ -10,9 +15,12 @@ class Post(Base):
     postid: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey(
         "users.id", onupdate="CASCADE", ondelete="CASCADE"))
+    display_id: Mapped[str] = mapped_column(ForeignKey(
+        "users.display_id", onupdate="CASCADE", ondelete="CASCADE"
+    ))
     content: Mapped[str] = mapped_column(String(500))
     favorite_count: Mapped[int] = mapped_column(default=0)
     repost_count: Mapped[int] = mapped_column(default=0)
 
     # なんもわからんけど動く
-    user = relationship("User")
+    user: Mapped["User"] = relationship("User", foreign_keys=[user_id])
