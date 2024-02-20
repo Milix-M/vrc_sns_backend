2b895c878c1485f407d6498cc45908512644a4f8
Milix-M <tujikou12dh@gmail.com>
2024-02-20 18:12:21 +0900

    FollowModelからuser_idを削除してfollowerとfollowingのリレーションを追加

--- api/db/models/user_follow_model.py
+++ api/db/models/user_follow_model.py
@@ -1,8 +1,11 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from api.db.base import Base
 
+if TYPE_CHECKING:
+    from api.db.models.user_model import User
 
 class FollowModel(Base):
     """Model for follow and follower."""
@@ -10,18 +13,16 @@ class FollowModel(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
-    user_id: Mapped[int] = mapped_column(ForeignKey(
-        "users.id", onupdate="CASCADE", ondelete="CASCADE")
-    )
-
     follower_id: Mapped[int] = mapped_column(ForeignKey(
         "users.id", onupdate="CASCADE", ondelete="CASCADE"),
         primary_key=True,
-        nullable=True,
     )
 
     following_id: Mapped[int] = mapped_column(ForeignKey(
         "users.id", onupdate="CASCADE", ondelete="CASCADE"),
         primary_key=True,
-        nullable=True,
     )
+
+
+    follower: Mapped["User"] = relationship("User", foreign_keys=[follower_id])
+    following: Mapped["User"] = relationship("User", foreign_keys=[following_id])
