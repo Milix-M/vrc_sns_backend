62b98f9df8a01eb0c585731af59ebdade8634f94
Milix-M <tujikou12dh@gmail.com>
2024-02-20 18:13:51 +0900

    followers_count, following_countカラムをUserモデルに追加

--- api/db/models/user_model.py
+++ api/db/models/user_model.py
@@ -23,6 +23,8 @@ class User(Base):
         Date())
     profile: Mapped[str | None]
     hashed_password: Mapped[str]
+    followers_count: Mapped[int] = mapped_column(default=0)
+    following_count: Mapped[int] = mapped_column(default=0)
     is_initialized: Mapped[bool] = mapped_column(default=False)
 
     @hybrid_method
